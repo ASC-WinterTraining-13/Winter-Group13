ae60f5e2df9860feb5b9626436cacffa9908abc8
YINGYYUN <756209834@qq.com>
2026-01-28 11:55:25 +0800

    fix:2026.1.28.11:51:测试了电机

--- project/code/motor.c
+++ project/code/motor.c
@@ -6,15 +6,17 @@ void motor_Init(void)//电机初始化
 	pwm_init(TIM5_PWM_CH4_A3, 17000, 0);//PWM端口设定为A1，A3
 	
 	gpio_init (A0, GPO, 0, GPO_PUSH_PULL);
-	gpio_init (A2, GPO, 0, GPO_PUSH_PULL);//电机1驱动端口设定为A0,A2
+	gpio_init (A2, GPO, 0, GPO_PUSH_PULL);//电机1(左)驱动端口设定为A0,A2
 	
-	gpio_init (D5, GPO, 0, GPO_PUSH_PULL);
-	gpio_init (D6, GPO, 0, GPO_PUSH_PULL);//电机2驱动端口设定为D5,D6
+	gpio_init (B10, GPO, 0, GPO_PUSH_PULL);
+	gpio_init (B11, GPO, 0, GPO_PUSH_PULL);//电机2（右）驱动端口设定为B10，B11
 }
 
 void motor_SetPWM(uint8_t motor_id, int PWM)
 {
-
+	if (PWM >  10000){PWM =  10000;}
+	if (PWM < -10000){PWM = -10000;}
+	
 	if (motor_id == 1)
 	{
 		if (PWM >= 0)
@@ -34,14 +36,14 @@ void motor_SetPWM(uint8_t motor_id, int PWM)
 	{
 		if (PWM >= 0)
 		{
-			gpio_set_level (D5,0);
-			gpio_set_level (D6,1);//正反转可调.
+			gpio_set_level (B10,0);
+			gpio_set_level (B11,1);//正反转可调.
 			pwm_set_duty(TIM5_PWM_CH4_A3,PWM);
 		}
 		else
 		{
-			gpio_set_level (D5,1);
-			gpio_set_level (D6,0);//正反转可调.
+			gpio_set_level (B10,1);
+			gpio_set_level (B11,0);//正反转可调.
 			pwm_set_duty(TIM5_PWM_CH4_A3,-PWM);
 		}
 	}
--- project/code/motor.h
+++ project/code/motor.h
@@ -4,7 +4,8 @@
 #include "zf_common_typedef.h"
 
 void motor_Init(void);
-// motor_id取1或2;PWM有效值-10000~10000
+// motor_id取1（左）或2（右）;
+//PWM有效值-10000~10000
 void motor_SetPWM(uint8_t motor_id, int PWM);
 
 #endif
